cd9d3f5ef0f65609b5156077389357b971a1755b
Lakshita Ganesh Kumar <lakshita15@outlook.com>
2025-06-05 20:47:27 -0400

    Initial Commit

---
+++ StyleQuiz/StyleQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleQuizApp.swift
+//  StyleQuiz
+//
+//  Created by Lakshita Ganesh Kumar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StyleQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StyleQuizTests/StyleQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleQuizTests.swift
+//  StyleQuizTests
+//
+//  Created by Lakshita Ganesh Kumar on 6/5/25.
+//
+
+import Testing
+@testable import StyleQuiz
+
+struct StyleQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
